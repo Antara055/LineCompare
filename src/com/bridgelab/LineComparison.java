96089fe1c85048db7e777dfc0e00b9d182325f17
Antara055 <antara55.dgp10.1434@gmail.com>
2021-10-23 17:18:09 +0530

    Added : taking co-ordinates points and calculating length

--- src/com/bridgelab/LineComparison.java
+++ src/com/bridgelab/LineComparison.java
@@ -1,7 +1,21 @@
-package com.bridgelab;
+package com.bridgelab;                                              //package
+import java.util.Scanner;                                          // include Scanner
 
-public class LineComparison {
+public class LineComparison {                                     //Class
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Compare");
-    }
+        System.out.println("Welcome to Line Compare Program");
+                int x1,y1;
+                int x2,y2;
+                Scanner sc =new Scanner(System.in);             //taking inputs using scanner
+                System.out.println("The coordinates are: ");    // x1,x2 and y1,y2 are co-ordinate points
+                System.out.print("x1 = ");x1=sc.nextInt();
+                System.out.print("y1 = ");y1=sc.nextInt();
+                System.out.print("x2 = ");x2=sc.nextInt();
+                System.out.print("y2 = ");y2=sc.nextInt();
+
+                double length = Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));  //calculating the length of the line
+                System.out.println("The length of the line is : "+length);
+
+
+            }
 }
